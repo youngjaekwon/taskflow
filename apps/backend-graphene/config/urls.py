19fe3ce97f6db73f357ea58423a20ee58e9b70c0
youngjaekwon <gczzga@gmail.com>
2026-02-13 18:37:58 +0900

    feat(backend-graphene): add media file serving in debug mode
    개발 환경에서 미디어 파일(프로필 이미지 등)을 서빙하기 위해
    DEBUG 모드일 때 MEDIA_URL 경로에 대한 static 서빙 설정 추가.

--- apps/backend-graphene/config/urls.py
+++ apps/backend-graphene/config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -28,3 +29,4 @@ if settings.DEBUG:
             name="redoc",
         ),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
